a674f02ee3c000b182a90083d3d08ae476dfb361
Jesun <Rodrigo.278483@pasay-edsa.sti.edu.ph>
2026-06-04 20:32:56 +0800

    Fix errors

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ import About from "./components/About"
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import Aos from "aos";
+import AOS from "aos";
 import "aos/dist/aos.css";
 
 const App = () => {
   useEffect(() => {
-    Aos.init({
+    AOS.init({
       duration: 1000,
       once: false,
       offset: 100,
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -47,7 +47,7 @@ const HeroSection = () => {
               {/* Download */}
               <button className="border border-cyan-400 px-6 md:px-7 py-3 rounded-full flex items-center justify-center gap-2 w-full sm:w-max hover:scale-105 transition-all">
                 <Download size={18} className="text-cyan-400" />
-                <a href="/resume.pdf" download className="text-cyan-400">
+                <a href="./resume.pdf" download className="text-cyan-400">
                   Download Resume
                 </a>
               </button>
